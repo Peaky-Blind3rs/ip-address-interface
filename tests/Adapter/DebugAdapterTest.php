1f03e179830d1cc36d7ca23a21749d8f3586c675
Tommy Shelby <dhruv@remitso.com>
2023-07-14 21:24:29 +0530

    Test Updated

--- tests/Adapter/DebugAdapterTest.php
+++ tests/Adapter/DebugAdapterTest.php
@@ -11,10 +11,8 @@ declare(strict_types=1);
 
 namespace PeakyBlind3rs\IpAddressInterface\Tests\Adapter;
 
-use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\TestCase;
 use PeakyBlind3rs\IpAddressInterface\Adapter\DebugAdapter;
-use PeakyBlind3rs\IpAddressInterface\Model\IpAddress;
 
 /**
  * Class DebugAdapterTest
@@ -27,9 +25,9 @@ use PeakyBlind3rs\IpAddressInterface\Model\IpAddress;
 class DebugAdapterTest extends TestCase
 {
     /**
-     * @var DebugAdapter
+     * @var \PeakyBlind3rs\IpAddressInterface\Adapter\DebugAdapter
      */
-    private $debugAdapter;
+    private DebugAdapter $debugAdapter;
 
     /**
      * {@inheritdoc}
@@ -49,6 +47,7 @@ class DebugAdapterTest extends TestCase
     public function testLookup(): void
     {
         $ipAddress = '127.0.0.1';
+        /** @var \PeakyBlind3rs\IpAddressInterface\Interface\Model\IpAddressInterface $ipAddressInstance */
         $ipAddressInstance = $this->debugAdapter->lookup('127.0.0.1');
 
         $this->assertEquals($ipAddress, $ipAddressInstance->getIp());
